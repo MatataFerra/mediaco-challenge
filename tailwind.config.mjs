d6084ee0234236c89efe541f9c43d47b8a9a47b9
matataferra <matias.agf@gmail.com>
2024-06-15 18:52:15 +1000

    feat: Add new components and styles for the landing page
    The code changes include adding new components and styles for the landing page. This commit introduces the following changes:
    
    - Added `Logo` component with specific styling
    - Added `Links` component with a list of links
    - Added `Header` component with a logo and navigation
    - Added `Service` component with a title and items
    - Added `Title` component with a styled heading
    - Added `Availability` component with availability information
    - Added `Input` component with customizable props
    - Added `Button` component with customizable props
    - Added `Query` component with a title, promotion phone, and query box
    - Added `Item` component with a title and description
    - Added `Card` component with customizable background and content slot
    - Added `Items` component with a list of items
    
    These changes enhance the landing page by introducing new components and styles to improve the overall user experience.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: "#fefefe",
+				bones: "#f1f1f1",
+				owl: "#231F20",
+				milk: "#efefef",
+				wall: "#e3e3e3",
+				ocean: "#0074ff",
+				"light-ocean": "#006aff",
+				"dark-ocean": "#004881",
+				lake: "#00aeff",
+				"dark-lake": "#003356",
+				banana: "#ffd100",
+				cricket: "#21ae2b",
+				facebook: "#0c6cb9",
+				fire: "#dd1717",
+			},
+		},
 	},
 	plugins: [],
 }
